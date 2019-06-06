51d1b3a2e1f18243a25e8ea0b1e7ea2c5206d839
Chris Engelhart <engleman@gmail.com>
2019-06-06 14:41:08 -0400

    Added shelving test

--- test.py
+++ test.py
@@ -2,9 +2,19 @@
 
 import tm
 import pprint
+import shelve
 
 with open('ABC Buckets - Sheet1.csv', newline='') as csvfile:
     mylist = tm.List('ABC Buckets', csvfile)
 
 with open('test.csv', 'w', newline='') as csvfile:
     mylist.export_csv(csvfile)
+
+with shelve.open('test.pyshelve') as shelf:
+    shelf['test'] = mylist
+
+with shelve.open('test.pyshelve') as s2:
+    unsh = s2['test']
+
+with open('t2.csv', 'w', newline='') as c2:
+    unsh.export_csv(c2)
