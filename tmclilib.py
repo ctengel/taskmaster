e91d442974ca1692428c4103a55243c950c2bec5
Chris Engelhart <engleman@gmail.com>
2021-07-01 06:13:09 -0400

    Adding support for many more fields
    Complex date handling
    Object oriented client library
    Part of #3
    Working towards modes

--- tmclilib.py
+++ tmclilib.py
@@ -2,6 +2,31 @@
 
 import requests
 
+# TODO: oo?
+
+class Task:
+    # TODO allow this to be used for new, arbitrary updates, etc
+    # TODO check for conflicts
+    # TODO triage
+    def __init__(self, api, dct):
+        self.api = api
+        self.dct = dct
+        self.tid = dct['id']
+    def export(self):
+        return self.dct
+    def close(self):
+        # TODO duplicate
+        self.api.close_task(tid)
+    def update(self, ptch):
+        self.dct = self.api.update_task(self.tid, ptch)
+    def warm(self, un=False):
+        # TODO set schedule to now if not yet...?
+        self.update({'warm': not un})
+    def schedule(self, when):
+        # TODO convert to string
+        # TODO allow NOW... or server side
+        self.update({'wakeup': when.isoformat()})
+
 
 class TMApi:
     def __init__(self, url):
@@ -22,18 +47,20 @@ class TMApi:
         r.raise_for_status()
         return r.json()
 
-    def new_task(self, name):
-        return self.post('tasks/', {'name': name})['id']
+    def new_task(self, td):
+        return Task(self, self.post('tasks/', td))
 
-    def all_tasks(self, basicfilt=None):
+    def all_tasks(self, mode=None):
         params = None
-        return self.get('tasks/', params)
+        if mode:
+            params = {'mode': mode}
+        return [Task(self, x) for x in self.get('tasks/', params)]
 
     def one_task(self, tid=None):
-        return self.get('tasks/' + str(tid))
+        return Task(self, self.get('tasks/' + str(tid)))
 
-    def update_task(self, tid, newname):
-        self.put('tasks/' + str(tid), {'name': newname})
+    def update_task(self, tid, newdata):
+        return self.put('tasks/' + str(tid), newdata)
 
     def close_task(self, tid, duplicate=False):
         res = self.post('tasks/' + str(tid) + '/action', {'close': True, 'duplicate': duplicate})
--- tmsqlapi.py
+++ tmsqlapi.py
@@ -12,7 +12,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/tmsql.db'
 db = flask_sqlalchemy.SQLAlchemy(app)
 
-api = flask_restx.Api(app, version='0.1', title='TaskMaster API', description='API for interacting with a TaskMaster DB')
+api = flask_restx.Api(app, version='0.1', title='TaskMaster API', description='API for interacting with a TaskMaster DB')#, validate=True) TODO validate
 
 taskns = api.namespace('tasks', description='TODO operations')
 
@@ -21,7 +21,14 @@ task = api.model('Task', {'id': flask_restx.fields.Integer(readonly=True, descri
     'name': flask_restx.fields.String(required=True, description='Short name of task'),
     'created':  flask_restx.fields.DateTime(readonly=True, description='When task created'),
     'closed': flask_restx.fields.DateTime(readonly=True, description='When task closed'),
-    'updated': flask_restx.fields.DateTime(readonly=True, description='When task updated')
+    'updated': flask_restx.fields.DateTime(readonly=True, description='When task updated'),
+    'urgent': flask_restx.fields.Boolean(description='Eisenhower urgency'),
+    'important': flask_restx.fields.Boolean(description='Eisenhower importance'),
+    'frog': flask_restx.fields.Boolean(description='Is it a frog?'),
+    'wakeup': flask_restx.fields.DateTime(description='When a task should be staged or slept until'),
+    'warm': flask_restx.fields.Boolean(description='Actively in execution'),
+    'pomodoros': flask_restx.fields.Integer(description='How long it take'),
+    'priority': flask_restx.fields.Integer(description='Priority')
     })
 
 action = api.model('Action', {'close': flask_restx.fields.Boolean(description='Close task', default=False), 'duplicate': flask_restx.fields.Boolean(description='Duplicate task', default=False)})
@@ -30,14 +37,14 @@ action = api.model('Action', {'close': flask_restx.fields.Boolean(description='C
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), nullable=False)
-    #priority ABC
-    #urgent = db.Column(db.Boolean)
-    #important = db.Column(db.Boolean)
-    #frog = db.Column(db.Boolean)
-    #pomodoros = db.Column(db.Integer)
-    #wakeup = db.Column(db.DateTime)
+    priority = db.Column(db.Integer)
+    urgent = db.Column(db.Boolean)
+    important = db.Column(db.Boolean)
+    frog = db.Column(db.Boolean, default=False)
+    pomodoros = db.Column(db.Integer)
+    wakeup = db.Column(db.DateTime)
     #description = db.Column(db.Text)
-    #warm = db.Column(db.Boolean)
+    warm = db.Column(db.Boolean, default=False)  #TODO should this be nullable
     created = db.Column(db.DateTime, default=datetime.datetime.now)
     updated = db.Column(db.DateTime, onupdate=datetime.datetime.now)
     closed = db.Column(db.DateTime)
@@ -49,18 +56,46 @@ class Task(db.Model):
     #goal = db.Column(db.String(16))
     #dependency_id = db.Column(db.Integer, db.ForeignKey('task.id'))
     #nexttasks = db.relationship('Person')
+    # TODO due??? handle overdue???
+
 
 @taskns.route('/')
 class TaskList(flask_restx.Resource):
+    # TODO swagger document mode param
     @taskns.doc('list_tasks')
     @taskns.marshal_list_with(task)
     def get(self):
-        return Task.query.all()
+        parser = flask_restx.reqparse.RequestParser() # TODO better way to call this?
+        parser.add_argument('mode', choices=('triage', 'schedule', 'stage', 'execute', 'all', 'open', 'closed'), default='open')
+        args = parser.parse_args()
+        mymo = args['mode']
+        assert mymo
+        if mymo == 'all':
+            return Task.query.all()
+        if mymo == 'open':
+            return Task.query.filter_by(closed=None).all()
+        if mymo == 'closed':
+            return Task.query.filter(Task.closed != None).order_by(Task.closed.desc()).all()
+        if mymo == 'execute':
+            return Task.query.filter_by(warm=True, closed=None).order_by(Task.frog.desc(), Task.priority).all()
+        if mymo == 'stage':
+            # TODO should this include warm?
+            return Task.query.filter(Task.closed == None, Task.wakeup <= datetime.datetime.now()).order_by(Task.frog.desc(), Task.priority, Task.urgent.desc(), Task.important.desc()).all()
+        if mymo == 'schedule':
+            # TODO also put in overdue
+            # TODO should this include current schedule
+            # TODO bulk reschedule option?
+            return Task.query.filter(Task.closed == None, Task.warm == False, Task.wakeup == None).order_by(Task.important.desc(), Task.urgent.desc()).all()
+        if mymo == 'triage':
+            # TODO look for missing tags here
+            return Task.query.filter(Task.closed == None, ((Task.pomodoros == None) | (Task.urgent == None) | (Task.important == None))).all()
+        assert False
 
     @taskns.doc('create_task')
     @taskns.expect(task)
     @taskns.marshal_with(task, code=201)
     def post(self):
+        # TODO restrict
         newtask = Task(**api.payload)
         db.session.add(newtask)
         db.session.commit()
@@ -81,8 +116,13 @@ class TodoOne(flask_restx.Resource):
     @taskns.marshal_with(task)
     def put(self, id):
         mytask = Task.query.get_or_404(id)
-        if 'name' in api.payload:
-            mytask.name = api.payload['name']
+        for key, value in api.payload.items():
+            if key in ['name', 'priority', 'urgent', 'important', 'frog', 'pomodoros', 'warm']:
+                setattr(mytask, key, value)
+            elif key in ['wakeup']: # TODO is this the best way?
+                setattr(mytask, key, datetime.datetime.fromisoformat(value)) # TODO timezone?
+            else:
+                flask_restx.abort(403)
         db.session.commit()
         return mytask
 
@@ -115,9 +155,7 @@ class TodoAction(flask_restx.Resource):
 
 # TODO get similar to task ID x
 # TODO search for string in name, desc, etc
-# TODO find triagable tasks
-# TODO find schedulable tasks
-# TODO find followupable tasks
+# TODO search for tags
 
 
 db.create_all()
